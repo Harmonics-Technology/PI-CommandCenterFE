d3120a368405cfe77b32be26a93aa74454e6305d
adelowomi <adelowomi@gmail.com>
2023-08-24 14:21:48 +0100

    Fixed pages

--- src/components/subpages/PricingPage.tsx
+++ src/components/subpages/PricingPage.tsx
@@ -118,8 +118,8 @@ export const PricingPage = ({ base }: ISubscriptionProps) => {
                                 ) as unknown as boolean
                             }
                             desc={x.description}
-                            prices={x.totalMonthlyAmount}
-                            price={x.totalYearlyAmount}
+                            prices={x.totalYearlyAmount}
+                            price={x.totalMonthlyAmount}
                             billed={billing == 'year' ? 'year' : 'month'}
                             recommended={x.recommendedFor}
                             features={x.features?.split(',').map((b) => b)}
